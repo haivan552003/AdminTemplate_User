43a7961d3042b5297ae465a240b0e23d3160dabf
May <83487199+haivan552003@users.noreply.github.com>
2024-05-20 09:34:41 +0700

    first commit

---
+++ AdminTemplate_User/AdminTemplate_User/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AdminTemplate_User.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
